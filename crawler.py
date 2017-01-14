fb629821defa78d0e1de41ab2b1e2ea87b445cf0
rg3915 <rg3915@yahoo.com.br>
2017-01-14 20:17:41 -0200

    função e get_html_link

--- crawler.py
+++ crawler.py
@@ -1,13 +1,22 @@
 import re
 import requests
 
-url = 'http://lacoxinha.com.br/contato'
 
-requisicao = requests.get(url)
+def get_url(url):
+    requisicao = requests.get(url)
+    padrao = re.findall(r'[\w\.-]+@[\w-]+.[\w\.-]+', requisicao.text)
+    if padrao:
+        return padrao
+    return 'Padrao nao encontrado'
 
-padrao = re.findall(r'[\w\.-]+@[\w-]+.[\w\.-]+', requisicao.text)
 
-if padrao:
-    print(padrao)
-else:
-    print('Padrao nao encontrado')
+def get_html_links(url):
+    ''' Retorna uma lista de links '''
+    links = re.findall(r'href=[\'"]?([^\'" >]+)', url)
+    return links
+
+
+if __name__ == '__main__':
+    URL = 'http://steltech.com.br/contato/'
+    print(get_url(URL))
+    print(get_html_links(URL))
